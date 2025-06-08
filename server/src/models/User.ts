57274e548b5f8dd21ffae2eb17d33f03746d0061
IKRAM-GRYACH <Ikramegryach@gmail.com>
2025-06-08 20:08:06 +0200

    ajustement hachage MDP

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,11 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+} from 'typeorm';
 import bcrypt from 'bcryptjs';
 
+/**
+ * Enum for user roles.
+ */
 export enum UserRole {
   CUSTOMER = 'customer',
-  PRODUCER = 'producer'
+  PRODUCER = 'producer',
 }
 
+/**
+ * User entity representing application users.
+ */
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('uuid')
@@ -20,7 +34,7 @@ export class User {
   @Column({
     type: 'enum',
     enum: UserRole,
-    default: UserRole.CUSTOMER
+    default: UserRole.CUSTOMER,
   })
   role: UserRole;
 
@@ -38,13 +52,31 @@ export class User {
   @UpdateDateColumn()
   updatedAt: Date;
 
-  // Method to hash password
-  async hashPassword(): Promise<void> {
-    this.password = await bcrypt.hash(this.password, 10);
+  /**
+   * Hash the password before saving if it has changed.
+   */
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPasswordIfNeeded(): Promise<void> {
+    if (this.password && !this.password.startsWith('$2a$')) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
   }
 
-  // Method to check password
+  /**
+   * Check if the provided password matches the stored hash.
+   * @param password Plain text password to check
+   * @returns Promise<boolean>
+   */
   async checkPassword(password: string): Promise<boolean> {
     return bcrypt.compare(password, this.password);
   }
-} 
+
+  /**
+   * Hide sensitive fields when returning user objects.
+   */
+  toJSON() {
+    const { password, ...user } = this;
+    return user;
+  }
+}
